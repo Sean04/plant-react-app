fcf4e3f2f3bb4a4097be04e2eb16353247cc4c2b
Sean04 <z5163023@unsw.edu.au>
2020-05-26 17:15:38 +1000

    Changed UI/UX design from Bootstrap to Material UI

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ import {
   Link
 } from "react-router-dom";
 
+// Material UI
+import { Grid } from '@material-ui/core';
+import MuiAppBar from './components/MuiAppBar';
+
+
 import AppNavbar from './components/AppNavbar';
 import Dashboard from './components/Dashboard';
 
@@ -14,22 +19,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
+    // <Router>
+    //   <div className="App">
+    //     <nav>
+    //       <AppNavbar />
+    //     </nav>
+    //     <Switch>
+    //       <Route path="/dashboard">
+    //         <Dashboard />
+    //       </Route>
+    //       <Route path="/plants">
+    //         <h1>Plants</h1>
+    //       </Route>
+    //       <Route path="/history">
+    //         <h1>Data</h1>
+    //       </Route>
+    //     </Switch>
+    //   </div>
+    // </Router>
+
     <Router>
       <div className="App">
-        <nav>
-          <AppNavbar />
-        </nav>
-        <Switch>
-          <Route path="/dashboard">
-            <Dashboard />
-          </Route>
-          <Route path="/plants">
-            <h1>Plants</h1>
-          </Route>
-          <Route path="/history">
-            <h1>Data</h1>
-          </Route>
-        </Switch>
+        <Grid container direction="column">
+          <Grid item>
+            <MuiAppBar />
+          </Grid>
+          <Grid item>
+            <Switch>
+              <Route path="/dashboard">
+                <Dashboard />
+              </Route>
+            </Switch>
+          </Grid>
+        </Grid>
       </div>
     </Router>
   );
--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,112 +1,130 @@
 import React, { useState } from 'react';
-import CanvasJSReact from '../canvasjs.react';
-import { Container, Row, Col, Button } from 'reactstrap';
-import SideNav from './SideNav';
-import axios from 'axios';
 
-var CanvasJS = CanvasJSReact.CanvasJS;
-var CanvasJSChart = CanvasJSReact.CanvasJSChart;
+// MaterialUI
+import { makeStyles } from '@material-ui/core/styles';
+import { 
+    Paper,
+    Container,
+    Button,
+    Grid,
+    Card,
+    CardContent,
+    Typography,
+    List,
+    ListItem,
+    ListItemText,
+    ListItemAvatar,
+    Divider
+} from '@material-ui/core';
 
+import { FaTemperatureHigh, FaSun } from 'react-icons/fa';
+import { GiZigzagLeaf, GiWaterDrop } from 'react-icons/gi';
+
+import LineChart from './LineChart';
+
+const useStyles = makeStyles({
+    root: {
+        padding: '30px 30px 30px 30px'
+    }
+})
 
 export default function Dashboard() {
-
+    // Note: For useState the initial state does not matter, only
+    // used for reference for variable names.
     const [data, setData] = useState({
-        temperature: 0,
-        humidity: 0,
-        moisture: 0,
-        light: 0,
-        soil_temperature: 0,
-        time: 0,
+        Temperature: 0,
+        Humidity: 0,
+        Soil_Moisture: 0,
+        Light: 0,
+        Date: Date.now()
     });
+    
+    const classes = useStyles();
 
+    var dataObjects = []
 
-    const options2 = {
-        animationEnabled: true,
-        exportEnabled: true,
-        theme: "dark2", // "light1", "dark1", "dark2"
-        title:{
-            text: "Soil Moisture"
-        },
-        axisY: {
-            title: "Moisture %",
-            includeZero: false,
-            suffix: "%"
-        },
-        axisX: {
-            title: "Time",
-            prefix: "",
-            interval: 2
-        },
-        data: [{
-            type: "line",
-            toolTipContent: "Time {x}: {y}%",
-            dataPoints: [
-                { x: 1, y: 64 },
-                { x: 2, y: 61 },
-                { x: 3, y: 64 },
-                { x: 4, y: 62 },
-                { x: 5, y: 64 },
-                { x: 6, y: 60 },
-                { x: 7, y: 58 },
-                { x: 8, y: 59 },
-                { x: 9, y: 53 },
-                { x: 10, y: 54 },
-                { x: 11, y: 61 },
-                { x: 12, y: 60 },
-                { x: 13, y: 55 },
-                { x: 14, y: 60 },
-                { x: 15, y: 56 },
-                { x: 16, y: 60 },
-                { x: 17, y: 59.5 },
-                { x: 18, y: 63 },
-                { x: 19, y: 58 },
-                { x: 20, y: 54 },
-                { x: 21, y: 59 },
-                { x: 22, y: 64 },
-                { x: 23, y: 59 }
-            ]
-        }]
+    var graphDataPoints = []
+
+    function assignDataPoints(item, index) {
+        var d = new Date(item.Date);
+        console.log(d);
+
+        console.log(typeof item.Date)
+        graphDataPoints.push({
+            x: item.Temperature,
+            y: item.Date
+        });
     }
 
     const updateData = () => {
-        fetch('http://localhost:5000/api/test_data')
+        fetch('http://localhost:5000/api/data')
         .then(response => response.json())
         .then(new_data => {
             console.log(new_data);
-            setData(new_data);
-            console.log('Plant Data Object');
-            // console.log(plant_data);
-        });
+            dataObjects = new_data;
+            setData(new_data[new_data.length-1]);
+            console.log(data)
 
-        // axios.get('http://localhost:5000/api/plant_data')
-        // .then((res) => {
-        //     console.log(res);
-        // });
+            dataObjects.forEach(assignDataPoints);
+            console.log(graphDataPoints);
+        });
     };
 
     return (
-        <div>
-            <Container>
-                <h1>Dashboard</h1>
-                <Row>
-                    <Col>
-                        <h2>Current Status:</h2>
-                        <ul>
-                            <li>Room Temperature: {data.temperature}</li>
-                            <li>Humidity: {data.humidity}</li>
-                            <li>Soil Temperature: {data.soil_temperature}</li>
-                            <li>Soil Moisture: {data.moisture}</li>
-                            <li>Light: {data.light}</li>
-                        </ul>
-                        <Button color="primary" onClick={updateData}>Update</Button>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <CanvasJSChart options={options2}></CanvasJSChart>
-                    </Col>
-                </Row>
-            </Container>
-        </div>
+        <Container >
+            <Paper className={classes.root} elevation={3}>
+                <Grid container spacing={2}>
+                    <Grid item xs={12}>
+                        <Typography variant="h2">Dashboard</Typography>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h4">Current Status:</Typography>
+                                    <List>
+                                        <ListItem>
+                                            <ListItemAvatar>
+                                                <FaTemperatureHigh/>
+                                            </ListItemAvatar>
+                                            <ListItemText primary="Room Temperature:" secondary={data.Temperature} />
+                                        </ListItem>
+                                        <Divider variant="inset" component="li" />
+                                        <ListItem>
+                                            <ListItemAvatar>
+                                                <GiZigzagLeaf />
+                                            </ListItemAvatar>
+                                            <ListItemText primary="Soil Temperature:" secondary={data.Humidity} />
+                                        </ListItem>
+                                        <Divider variant="inset" component="li" />
+                                        <ListItem>
+                                            <ListItemAvatar>
+                                                <GiWaterDrop />
+                                            </ListItemAvatar>
+                                            <ListItemText primary="Soil Moisture:" secondary={data.Humidity} />
+                                        </ListItem>
+                                        <Divider variant="inset" component="li" />
+                                        <ListItem>
+                                            <ListItemAvatar>
+                                                <FaSun />
+                                            </ListItemAvatar>
+                                            <ListItemText primary="Light:" secondary={data.Humidity} />
+                                        </ListItem>
+
+                                    </List>
+                                <p>Last updated: {data.Date}</p>
+                                <Button color="primary" onClick={updateData}>Update</Button>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Card>
+                            <CardContent>
+                                <LineChart />
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                </ Grid>
+            </Paper>
+        </Container>
     )
 }
